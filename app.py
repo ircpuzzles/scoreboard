2d79e0682a4df97ba8898444ace0e54d2f930ab0
Vacation9 <samuel.damashek@gmail.com>
2014-05-01 21:33:39 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, flash
+from hashlib import sha256
+from database import *
+import string,random
+import re
+
+app = Flask(__name__)
+app.debug = True
+nickserv_regex = re.compile(r'[^ ]+')
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/register/',methods=['GET','POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    elif request.method == 'POST':
+        if request.form['password'] != request.form['confirm_password']:
+            flash('<div class="alert alert-danger">Password confirmation does not match</div>')
+            return render_template('register.html')
+        if not nickserv_regex.findall(request.form['account']):
+            flash('<div class="alert alert-danger">Invalid NickServ account</div>')
+            return render_template('register.html')
+        users = session.query(User).filter(User.account == request.form['account']).filter(User.confirmed == True).count()
+        if users > 0:
+            flash('<div class="alert alert-danger">A user is already registered with this NickServ account. Please try again.</div>')
+            return render_template('register.html')
+        password = sha256(request.form['password']).hexdigest()
+        confirmation_code = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
+        u = User(account=account, password=password, confirmation_code=confirmation_code)
+        session.add(u)
+        session.commit()
+        flash('<div class="alert alert-success">Registration successful.</div>')
+        return render_template('register.html',{'submitted':True,'confirmation_code':confirmation_code})
+        
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ database.py
@@ -0,0 +1,47 @@
+from sqlalchemy import create_engine, orm, Column, Integer, String, Boolean, ForeignKey, DateTime, func
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql://ircpuzzles:ircpuzzles@localhost/ircpuzzles')
+session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+Base = declarative_base()
+Base.query = session.query_property()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    account = Column(String)
+    confirmed = Column(Boolean)
+    password = Column(String)
+    confirmation_code = Column(String)
+
+    def __repr__(self):
+        return "<User(account='%s', confirmed='%s')>" % (self.account, str(self.confirmed))
+
+class GameInfo(Base):
+    """Information about running game, used by the bot and webapp 
+    to create the Game() instances.
+    """
+    __tablename__ = 'game_infos'
+
+    id = Column(Integer, primary_key=True)
+
+    # absolute path to the game.json
+    path = Column(String(512))
+    running = Column(Boolean)
+
+    def __repr__(self):
+        return "<GameInfo(%d, path='%s', running='%s')>" % (self.id, self.path, str(self.running))
+
+class Join(Base):
+    __tablename__ = 'joins'
+
+    id = Column(Integer, primary_key=True)
+    user = Column(Integer, ForeignKey('users.id'))
+    channel = Column(String)
+    time = Column(DateTime, default=func.now())
+
+
+def init_db():
+    Base.metadata.create_all(engine)
