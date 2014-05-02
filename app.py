e174c00dc4c9182a84f065aca54a9da9e204c8c8
Vacation9 <samuel.damashek@gmail.com>
2014-05-01 21:49:10 -0400

    Flashing improvement

--- app.py
+++ app.py
@@ -19,21 +19,21 @@ def register():
         return render_template('register.html')
     elif request.method == 'POST':
         if request.form['password'] != request.form['confirm_password']:
-            flash('<div class="alert alert-danger">Password confirmation does not match</div>')
+            flash(('danger','Password confirmation does not match'))
             return render_template('register.html')
         if not nickserv_regex.findall(request.form['account']):
-            flash('<div class="alert alert-danger">Invalid NickServ account</div>')
+            flash(('danger','Invalid NickServ account'))
             return render_template('register.html')
         users = session.query(User).filter(User.account == request.form['account']).filter(User.confirmed == True).count()
         if users > 0:
-            flash('<div class="alert alert-danger">A user is already registered with this NickServ account. Please try again.</div>')
+            flash(('danger','A user is already registered with this NickServ account. Please try again.'))
             return render_template('register.html')
         password = sha256(request.form['password']).hexdigest()
         confirmation_code = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
         u = User(account=request.form['account'], password=password, confirmation_code=confirmation_code)
         session.add(u)
         session.commit()
-        flash('<div class="alert alert-success">Registration successful.</div>')
+        flash(('success','Registration successful.'))
         return render_template('register.html',submitted=True,confirmation_code=confirmation_code)
         
 
